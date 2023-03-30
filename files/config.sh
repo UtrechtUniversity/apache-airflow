9b778f6f4b32cf5111086747fa1af70c50942e8b
Adam Kassimi <23499836+adamkasmi@users.noreply.github.com>
2023-03-30 16:00:57 +0200

    Create config.sh

---
+++ files/config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+if [ -d "/root/airflow/" ]; then
+
+    if [ 1 ]; then 
+
+    sudo sed -i '/executor = SequentialExecutor/ s/SequentialExecutor/LocalExecutor/' /root/airflow/airflow.cfg;
+    sudo sed -i "s|sqlite:////var/www/airflow//airflow.db|postgresql+psycopg2://airflow:airflow@localhost/airflow|g" /root/airflow/airflow.cfg;      
+    else
+    :
+
+    fi
+    sudo airflow scheduler
+
+fi
