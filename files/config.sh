5976bca48ecb17fd45712d64f566a952c43c7257
Adam Kassimi <23499836+adamkasmi@users.noreply.github.com>
2023-03-31 15:41:37 +0200

    Update config.sh

--- files/config.sh
+++ files/config.sh
@@ -5,7 +5,7 @@ if [ -d "/var/www/airflow/" ]; then
     if [ 1 ]; then 
     sudo sed -i '/load_examples = True/ s/True/False/' /var/www/airflow/airflow.cfg
     sudo sed -i '/executor = SequentialExecutor/ s/SequentialExecutor/LocalExecutor/' /var/www/airflow/airflow.cfg
-    sudo sed -i "s|sqlite:////root/airflow/airflow.db|postgresql+psycopg2://airflow:airflow@localhost/airflow|g" /var/www/airflow/airflow.cfg      
+    sudo sed -i "s|sqlite:////var/www/airflow/airflow.db|postgresql+psycopg2://airflow:airflow@localhost/airflow|g" /var/www/airflow/airflow.cfg      
     else
     :
 
